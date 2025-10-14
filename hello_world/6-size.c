0081f9cb6cb81512aaade573df619e008794345f
Emilien-Marot <12394@holbertonstudents.com>
2025-10-14 08:07:31 +0000

    exercise 1.6.2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	string a = "Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)";
+	char a[] = "Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n";
 
 	printf(a, sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
 	return (0);
