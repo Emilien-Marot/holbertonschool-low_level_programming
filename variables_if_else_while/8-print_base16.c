e0971df8889d92351e93c1d72e146ad814e6044b
Emilien-Marot <12394@holbertonstudents.com>
2025-10-14 10:53:50 +0000

    exercise 2.8.3

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n >= '9'; n--)
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
-	for (n = 'a'; n >= 'f'; n++)
+	for (n = 'a'; n <= 'f'; n++)
 		putchar(n);
 	putchar('\n');
 	return (0);
