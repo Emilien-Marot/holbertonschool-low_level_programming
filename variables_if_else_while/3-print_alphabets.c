e98b63fd1f2fef146e053f4693eb129ac5a92991
Emilien-Marot <12394@holbertonstudents.com>
2025-10-14 10:33:11 +0000

    exercise 2.3.1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - blabla
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
