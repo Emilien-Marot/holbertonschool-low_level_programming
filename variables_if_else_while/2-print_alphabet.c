7d7e2d9ad7a61fa47fc70ea5fcdae85140c1f89c
Emilien-Marot <12394@holbertonstudents.com>
2025-10-14 10:29:43 +0000

    exercise 2.2.1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - blabla
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
