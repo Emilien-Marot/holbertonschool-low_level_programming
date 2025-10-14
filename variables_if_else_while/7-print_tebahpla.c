416a3f12d4c052058f64ffb6c61986900d348723
Emilien-Marot <12394@holbertonstudents.com>
2025-10-14 10:48:30 +0000

    exercise 2.7.1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - blabla
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
