a329b1766c3a08ee0f83029c5cfcd391e1aa5f77
Emilien-Marot <12394@holbertonstudents.com>
2025-10-15 07:25:36 +0000

    exercise 3.6.1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - blabla
+ *
+ * @n: value
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
