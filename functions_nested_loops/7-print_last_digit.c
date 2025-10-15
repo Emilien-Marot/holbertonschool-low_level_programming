ca0209332df8ab7e85a2eca001b8543598bad136
Emilien-Marot <12394@holbertonstudents.com>
2025-10-15 07:49:32 +0000

    exercise 3.7.3

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,11 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = -1 * n;
+	int n2 = n % 10;
 
-	_putchar ('0' + (n % 10));
-	return (n % 10);
+	if (n2 < 0)
+		n2 = -1 * n2;
+
+	_putchar ('0' + n2);
+	return (n2);
 }
