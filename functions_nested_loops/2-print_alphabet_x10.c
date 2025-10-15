5c3528427ed879e36a77c81b08400a78434610f9
Emilien-Marot <12394@holbertonstudents.com>
2025-10-15 06:54:52 +0000

    exercise 3.2.1

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - blabla
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
