5e2d62d1bdbc144e043da24b27c9ae39b23f6e6b
Emilien-Marot <12394@holbertonstudents.com>
2025-10-15 07:05:33 +0000

    exercise 3.3.1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - blabla
+ *
+ * @c: value
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
