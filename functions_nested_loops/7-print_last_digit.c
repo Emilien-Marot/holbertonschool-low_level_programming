071a68c93b1b2b7d418c576f250e9bd31aa2e041
Emilien-Marot <12394@holbertonstudents.com>
2025-10-15 07:41:19 +0000

    exercise 3.7.1

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - blabla
+ *
+ * @n: value
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int print_last_digit(int n)
+{
+	_putchar (n % 10);
+	return (n % 10);
+}
