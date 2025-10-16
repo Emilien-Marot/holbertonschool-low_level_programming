e1846b477af106265d920e3374e0c36ef46570bd
Emilien-Marot <12394@holbertonstudents.com>
2025-10-16 06:33:18 +0000

    exercise 4.9.0

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - blabla
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz");
+			else
+				printf("Fizz");
+		}
+		else
+		{
+			if (i % 5 == 0)
+				printf("Buzz");
+			else
+				printf("%d", i);
+		}
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
