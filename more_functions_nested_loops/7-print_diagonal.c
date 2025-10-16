a1994033389d5d7b8edfe4f1e16f2a65cda36dfe
Emilien-Marot <12394@holbertonstudents.com>
2025-10-16 06:13:15 +0000

    exercise 4.7.0

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - blabla
+ *
+ * @n: abc
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
