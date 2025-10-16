6cd6c9b0ea5e2c5b6399898c027b77493cb00072
Emilien-Marot <12394@holbertonstudents.com>
2025-10-16 06:04:34 +0000

    exercise 4.6.0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - blabla
+ *
+ * @n: abc
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
