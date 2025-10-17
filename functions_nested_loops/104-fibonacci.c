28eeb9bf24e746a93756e67894a69f66e9c145f7
Emilien-Marot <12394@holbertonstudents.com>
2025-10-17 06:22:43 +0000

    exercise 3.16.1

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,8 +8,12 @@
  */
 int main(void)
 {
-	unsigned long int sum;
+	unsigned long int sum1;
+	unsigned long int sum10;
+	unsigned long int max = 10000000000000000;
+	unsigned long int n10 = 0;
 	unsigned long int n1 = 1;
+	unsigned long int n20 = 0;
 	unsigned long int n2 = 2;
 	int i;
 
@@ -17,10 +21,16 @@ int main(void)
 	{
 		if (i != 0)
 			printf(", ");
-		printf("%lu", n1);
-		sum = n1 + n2;
+		if (n10 > 0)
+			printf("%lu%lu", n10, n1);
+		else
+			printf("%lu", n1);
+		sum10 = n10 + n20 + ((n1 + n2) / max);
+		sum1 = (n1 + n2) % max;
 		n1 = n2;
-		n2 = sum;
+		n10 = n20;
+		n2 = sum1;
+		n20 = sum10;
 	}
 	printf("\n");
 	return (0);
