a2ff8c21dccb29f7ae6ba1c1a2455337c5cd8659
Emilien-Marot <12394@holbertonstudents.com>
2025-10-21 10:18:15 +0000

    exercise 9.7.0

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - blabla
+ *
+ * @str: abcd
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int l_max;
+	char l;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+		l_max = len / 2;
+	else
+		l_max = (len + 1) / 2;
+	for (l = len - l_max; l < len; l++)
+	{
+		_putchar(*(str + l));
+	}
+	_putchar('\n');
+}
