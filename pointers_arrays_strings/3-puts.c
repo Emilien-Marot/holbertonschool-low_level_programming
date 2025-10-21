9ba1f2e9794088a112a4279b19c1488d56c7c7a3
Emilien-Marot <12394@holbertonstudents.com>
2025-10-21 09:26:29 +0000

    exercise 9.3.0

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - blabla
+ *
+ * @str: abcd
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void _puts(char *str)
+{
+	int ch = 0;
+
+	while (*(str + ch) != '\0')
+	{
+		_putchar(*(str + ch));
+		ch++;
+	}
+	_putchar('\n');
+}
