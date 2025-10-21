ed542cdf63f8c1ca25fd5cd8bbc84b143ed6e514
Emilien-Marot <12394@holbertonstudents.com>
2025-10-21 07:53:50 +0000

    exercise 9.1.0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - blabla
+ *
+ * @a: abc
+ * @b: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
