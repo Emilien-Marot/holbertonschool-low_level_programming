023f9e45c5db456191eae5801030cfb01e1e96ac
Emilien-Marot <12394@holbertonstudents.com>
2025-10-22 06:16:24 +0000

    exercise 10.0.1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strcat - blabla
  *
@@ -15,13 +15,14 @@ char *_strcat(char *dest, char *src)
 	int len_s = 0;
 	int i;
 
-	while (*(dest + len_d) != '\0' && *(src + len_s) != '\0')
+	while (*(dest + len_d) != '\0' || *(src + len_s) != '\0')
 	{
 		if (*(dest + len_d) != '\0')
 			len_d++;
 		if (*(src + len_s) != '\0')
 			len_s++;
 	}
+	printf("%d-%d\n", len_d, len_s);
 	for (i = 0; i <= len_s; i++)
 	{
 		*(dest + len_d + i) = *(src + i);
