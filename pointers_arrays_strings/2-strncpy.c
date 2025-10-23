15f57b99efff97633e8003ca4271a88223473ac7
Emilien-Marot <12394@holbertonstudents.com>
2025-10-23 05:37:11 +0000

    exercise 10.2.2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
 	}
-	dest[i] = src[i];
 	return (dest);
 }
