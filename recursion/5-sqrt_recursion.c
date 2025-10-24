16ca737404dc3628d39ac8f129d9aa85bf365e0e
Emilien-Marot <12394@holbertonstudents.com>
2025-10-24 10:35:51 +0000

    exercise 12.5.0

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,5 +10,26 @@
  */
 int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	return (_sqrt_recursion_2(n, 0));
+}
+
+/**
+ * _sqrt_recursion_2 - blabla
+ *
+ * @n: abc
+ * @guess: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int _sqrt_recursion_2(int n, int guess)
+{
+	if (n == guess * guess)
+		return (guess);
+	else if (n < guess * guess)
+		return (-1);
+	else
+		return (_sqrt_recursion_2(n, guess + 1));
 }
