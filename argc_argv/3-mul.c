16c4da31da216c31021c4c2ab64fe6f251477d05
Emilien-Marot <12394@holbertonstudents.com>
2025-10-28 06:13:35 +0000

    exercise 13.3.0

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - blabla
+ *
+ * @argc: abc
+ * @argv: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if(argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
