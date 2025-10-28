f3f30a6a45994de656afa84436a0b27308b6d869
Emilien-Marot <12394@holbertonstudents.com>
2025-10-28 05:35:57 +0000

    exercise 13.0.0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - blabla
+ *
+ * @argc: abc
+ * @argv: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
