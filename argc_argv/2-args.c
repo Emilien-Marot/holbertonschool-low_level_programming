5c9752d0b15c6c955635b87b250fb3af5074c08e
Emilien-Marot <12394@holbertonstudents.com>
2025-10-28 06:00:54 +0000

    exercise 13.2.0

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - blabla
+ *
+ * @argc: abc
+ * @argv: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
