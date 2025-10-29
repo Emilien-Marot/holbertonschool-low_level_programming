09eccb3c4e9ee23f9e43ffb65ced05ac1cbd7866
Emilien-Marot <12394@holbertonstudents.com>
2025-10-29 11:01:18 +0000

    exercise 14.3.0

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - blabla
+ *
+ * @width: abc
+ * @height: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+		return (NULL);
+}
