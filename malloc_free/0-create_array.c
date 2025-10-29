4166950dcae034431c244656d47b00177347abfd
Emilien-Marot <12394@holbertonstudents.com>
2025-10-29 09:46:11 +0000

    exercise 14.0.0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - blabla
+ *
+ * @size: abc
+ * @c: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *table;
+
+	if (size == 0)
+		return (NULL);
+	table = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		table[i] = c;
+	}
+	return (table);
+}
