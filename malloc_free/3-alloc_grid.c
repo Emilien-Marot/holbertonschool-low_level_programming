3e504036a871639880aee1bda76cb92c21b006ed
Emilien-Marot <12394@holbertonstudents.com>
2025-10-30 06:08:42 +0000

    exercise 14.3.3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,17 +14,25 @@ int **alloc_grid(int width, int height)
 {
 	int **array;
 	int i;
+	int j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = calloc(height, sizeof(int *));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		array[i] = calloc(width, sizeof(int));
+		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
 	}
 	return array;
 }
