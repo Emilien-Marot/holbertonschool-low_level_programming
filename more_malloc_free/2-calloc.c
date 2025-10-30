c7495a7c165c8f3d0a222f01b26d46617cb02c07
Emilien-Marot <12394@holbertonstudents.com>
2025-10-30 10:27:33 +0000

    exercise 14.2.0

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - blabla
+ *
+ * @nmemb: abc
+ * @size: def
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *res;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return(NULL);
+	res = malloc(nmemb * size);
+	if (res == NULL)
+		return(NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		res[i] = 0;
+	}
+	return (res);
+}
