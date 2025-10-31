837212f7d0b5042f2b99cb1f53719dcbcafa4f22
Emilien-Marot <12394@holbertonstudents.com>
2025-10-31 07:28:57 +0000

    exercise 16.4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * new_dog - blabla
@@ -14,12 +15,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t res;
-	dog_t *ptr;
-
-	ptr = &res;
+	dog_t *ptr = malloc(sizeof(dog_t));
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
-	return (ptr);
+	return ptr;
 }
