c84c768fc162140088fb8a71a24184d70c5fa7bd
Emilien-Marot <12394@holbertonstudents.com>
2025-10-31 06:08:12 +0000

    exercise 16.1.0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - blabla
+ *
+ * @d: abc
+ * @name: def
+ * @age: ghi
+ * @owner: jkl
+ *
+ * Description: blabla
+ * Return: blabla
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
