fcb9f07a273448507d74cad3dcb67fd58abfcd82
Emilien-Marot <12394@holbertonstudents.com>
2025-10-31 10:14:33 +0000

    exercise 16.4.3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,52 +4,31 @@
 #include <stdlib.h>
 
 /**
- * _strlen - blabla
+ * _strdup - blabla
  *
- * @s: abcd
- *
- * Description: blabla
- * Return: blabla
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-	{
-		len++;
-	}
-return (len);
-}
-
-/**
- * _strncpy - blabla
- *
- * @dest: abc
- * @src: def
- * @n: ghi
+ * @str: abc
  *
  * Description: blabla
  * Return: blabla
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strdup(char *str)
 {
-	int i;
-	char s = 1;
+	unsigned int i;
+	unsigned int count = 0;
+	char *str2;
 
-	for (i = 0; i < n; i++)
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	str2 = malloc((count + 1) * sizeof(char));
+	if (str2 == NULL)
+		return (NULL);
+	for (i = 0; i <= count; i++)
 	{
-		if (s && src[i] != '\0')
-		{
-			dest[i] = src[i];
-		}
-		else
-		{
-			s = 0;
-			dest[i] = '\0';
-		}
+		str2[i] = str[i];
 	}
-	return (dest);
+	return (str2);
 }
 
 /**
@@ -65,23 +44,18 @@ char *_strncpy(char *dest, char *src, int n)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr = malloc(sizeof(dog_t));
+
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = malloc(_strlen(name) + 1);
+	ptr->name = _strdup(name);
 	if (ptr->name == NULL)
-	{
 		free(ptr);
-		return (NULL);
-	}
-	ptr->owner = malloc(_strlen(name) + 1);
+	ptr->owner = _strdup(owner);
 	if (ptr->owner == NULL)
 	{
 		free(ptr->name);
 		free(ptr);
-		return (NULL);
 	}
-	_strncpy(ptr->name, name, _strlen(name)+1);
-	_strncpy(ptr->owner, owner, _strlen(owner)+1);
 	ptr->age = age;
-	return ptr;
+	return (ptr);
 }
