727dda459c6708fbba103e6ca2f36bb8592c2394
Emilien-Marot <12394@holbertonstudents.com>
2025-11-05 10:19:50 +0000

    18.3.0.2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,6 +13,7 @@
 void print_s(va_list ap)
 {
 	char *str = va_arg(ap, char *);
+
 	if (str == NULL)
 	{
 		printf("(nil)");
@@ -44,7 +45,7 @@ void print_i(va_list ap)
  */
 void print_f(va_list ap)
 {
-        printf("%f", va_arg(ap, double));
+	printf("%f", va_arg(ap, double));
 }
 
 /**
@@ -57,27 +58,13 @@ void print_f(va_list ap)
  */
 void print_c(va_list ap)
 {
-        printf("%c", va_arg(ap, int));
+	printf("%c", va_arg(ap, int));
 }
 
 /**
- * print_0 - blabla
+ * print_all - blabla
  *
- * @ap: abc
- *
- * Description: blabla
- * Return: blabla
- */
-void print_0(va_list ap)
-{
-	ap = ap;
-}
-
-/**
- * print_strings - blabla
- *
- * @separator: abc
- * @n: def
+ * @format: abc
  *
  * Description: blabla
  * Return: blabla
@@ -85,25 +72,29 @@ void print_0(va_list ap)
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int i = 0, j;
+	int i = 0, j = 0;
 	t_f list_type[] = {
 		{'s', print_s},
 		{'f', print_f},
 		{'i', print_i},
 		{'c', print_c},
-		{'\0', print_0}
+		{'\0', NULL}
 	};
 
-	va_start(ap, 0);
-	while(format[i] != '\0')
+	va_start(ap, format);
+	while (format[i] != '\0')
 	{
 		j = 0;
-		while(list_type[j].t != '\0' && list_type[j].t != format[i])
+		while (list_type[j].t != '\0' && list_type[j].t != format[i])
 		{
 			j++;
 		}
-		list_type[j].f(ap);
+		if (i != 0 && j != 4)
+			printf(", ");
+		if (j != 4)
+			list_type[j].f(ap);
 		i++;
 	}
+	printf("\n");
 	va_end(ap);
 }
