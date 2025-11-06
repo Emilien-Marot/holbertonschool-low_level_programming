a46649ff2e8a22b63179ddc7a994cff8459c433f
Emilien-Marot <12394@holbertonstudents.com>
2025-11-06 09:45:23 +0000

    10.9.1

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -11,16 +11,19 @@
 void print_number(int n)
 {
 	int div = 1;
+	unsigned int abs_n;
 
 	if (n < 0)
 	{
-		n = n * -1;
+		abs_n = -1 * n;
 		_putchar('-');
 	}
-	while (n / div >= 10)
+	else
+		abs_n = n;
+	while (abs_n / div >= 10)
 		div = div * 10;
 	for (; div >= 1; div = div / 10)
 	{
-		_putchar('0' + ((n / div) % 10));
+		_putchar('0' + ((abs_n / div) % 10));
 	}
 }
