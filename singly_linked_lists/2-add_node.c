d85b232c8a9fa9e1d0d88bb28e3b5c64f3356fbc
Emilien-Marot <12394@holbertonstudents.com>
2025-12-03 10:10:01 +0000

    exercise 20.2.3

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,15 +14,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *new_str = strdup(str);
-	int len = strlen(str);
+	int len = 0;
 	list_t *new = (list_t *)malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
+	while(new_str[len] != '\0')
+		len++;
 	new->str = new_str;
 	new->len = len;
 	new->next = *head;
 	*head = new;
-	free(new);
 	return (new);
 }
