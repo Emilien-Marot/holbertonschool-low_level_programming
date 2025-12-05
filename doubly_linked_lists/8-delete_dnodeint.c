176b38220f6c6ad346cd68c1eae04a55a53da2d1
Emilien-Marot <12394@holbertonstudents.com>
2025-12-05 06:38:59 +0000

    exercise 21.8.0

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - blabla
+ *
+ * @h: abc
+ * @index: def
+ *
+ * Return: xyz
+**/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current = *head;
+	dlistint_t *prev = NULL;
+	dlistint_t *next = NULL;
+
+	if (current == NULL)
+		return (-1);
+	for (i = 0; i < index; i++)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
+	prev = current->prev;
+	next = current->next;
+	free (current);
+	if (index == 0)
+		*head = next;
+	if (next != NULL)
+		next->prev = prev;
+	if (prev != NULL)
+		prev->next = next;
+	return (1);
+}
