891abf254c6e6bb223a4016720b48af0f672f2c7
Emilien-Marot <12394@holbertonstudents.com>
2025-12-09 09:51:51 +0000

    exercise 22.1.1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,12 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 	res = 1;
-	for(; text_content[i] != '\0';)
-		i++;
 	if (text_content != NULL)
+	{
+		for (; text_content[i] != '\0';)
+			i++;
 		res = write(file, text_content, i);
+	}
 	close(file);
 	if (res == -1)
-		return(-1);
+		return (-1);
 	return (1);
 }
