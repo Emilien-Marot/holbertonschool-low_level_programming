e818692e7ec33ca42af5d64c1a1a67f93d44d94c
Emilien-Marot <12394@holbertonstudents.com>
2025-12-09 10:55:44 +0000

    exercise 22.3.0

---
+++ file_io/3-cp.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - blabla
+ *
+ * @argc: abc
+ * @argv: def
+ *
+ * Return: xyz
+**/
+int main(int argc, char *argv[])
+{
+	//verify the number of parameters
+	//open file_from
+	//read file_from
+	//confirm that you read from file_from
+	//open file_to in create and truncate with mode 0664
+	//test if created
+	//write
+}
