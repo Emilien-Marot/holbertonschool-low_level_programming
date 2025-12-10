d3c7e6837cb0382386fd25c7ca11098b52a64515
Emilien-Marot <12394@holbertonstudents.com>
2025-12-10 07:55:15 +0000

    exercise 22.3.0

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,45 @@
 #define MAXBUFLEN 1024
 
 /**
+ * err_read - blabla
+ *
+ * @name: abc
+ *
+ * Return: xyz
+**/
+void err_read(char *name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+	exit(98);
+}
+
+/**
+ * err_close - blabla
+ *
+ * @fd: abc
+ *
+ * Return: xyz
+**/
+void err_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+	exit(100);
+}
+
+/**
+ * err_write - blabla
+ *
+ * @name: abc
+ *
+ * Return: xyz
+**/
+void err_write(char *name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", name);
+	exit(99);
+}
+
+/**
  * main - blabla
  *
  * @argc: abc
@@ -21,47 +60,31 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
+		err_read(argv[1]);
 	file_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		close(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit (99);
+		err_write(argv[2]);
 	}
-	do
-	{
+	do {
 		size = read(file_from, buf, sizeof(buf));
 		if (size == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit (98);
-		}
+			err_read(argv[1]);
 		if (dprintf(file_to, "%s", buf) == -1)
 		{
 			close(file_from);
 			close(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			exit (99);
+			err_write(argv[2]);
 		}
-	}
-	while (size == MAXBUFLEN);
+	} while (size == MAXBUFLEN);
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
-		exit (100);
-	}
+		err_close(file_to);
 	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
-		exit (100);
-	}
+		err_close(file_from);
 	return (1);
 }
