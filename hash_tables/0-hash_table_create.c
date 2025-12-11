368d0384bd54e726bb7d1caf1cfa7e5897551bda
Emilien-Marot <12394@holbertonstudents.com>
2025-12-11 07:26:59 +0000

    exercise 23.0.0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *res;
+
+	res = (hash_table_t *)malloc(size * sizeof(hash_table_t));
+	return(res);
+}
