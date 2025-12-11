285726b782b6e0b1e9254c3b6e55cf868789670e
Emilien-Marot <12394@holbertonstudents.com>
2025-12-11 11:07:36 +0000

    exercise 23.3.0

--- hash_tables/3-main.c
+++ hash_tables/3-main.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	unsigned long int i;
 	hash_table_t *ht;
-	hash_node_t *node;
+	/*hash_node_t *node;*/
 	char *key1 = "hetairas";
 	char *key2 = "mentioner";
 	char *val1 = "h";
@@ -21,13 +21,17 @@ int main(void)
 	ht = hash_table_create(1024);
 	hash_table_set(ht, key1, val1);
 	hash_table_set(ht, key2, val2);
-
-	i = key_index((unsigned char *)key1, 1024);
-	node = (ht->array)[i];
-	while(node != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("%s:%s:%p\n", node->key, node->value, (void *)node->next);
-		node = node->next;
+		printf("[%lu]\n", i);
+		/*node = (ht->array)[i];
+		if (node == NULL)
+			 printf("[%lu]\n", i);
+		while(node != NULL)
+		{
+			printf("[%lu] %s:%s:%p\n", i, node->key, node->value, (void *)node->next);
+			node = node->next;
+		}*/
 	}
 	return (EXIT_SUCCESS);
 }
