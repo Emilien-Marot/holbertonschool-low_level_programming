fcdd0c6457f3972e88e1bc14e9a87002c1f55a19
Emilien-Marot <12394@holbertonstudents.com>
2025-12-11 08:42:17 +0000

    exercise 23.0.1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,9 +1,27 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - blabla
+ *
+ * @size: abd
+ *
+ * Return: xyz
+**/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *res;
+	hash_node_t *node;
+	hash_table_t *table;
 
-	res = (hash_table_t *)malloc(size * sizeof(hash_table_t));
-	return (res);
+	node = malloc(size * sizeof(void *));
+	if(node == NULL)
+		return(NULL);
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if(table == NULL)
+	{
+		free(node);
+                return(NULL);
+	}
+	table->size = size;
+	table->array = &node;
+	return (table);
 }
